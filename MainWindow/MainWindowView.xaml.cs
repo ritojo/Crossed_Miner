57e171a8751bf882104a0494802810dd5e412585
Zachary Trahan <zacktrahan15@gmail.com>
2021-02-02 16:38:32 -0700

    Put MVVM directory structure in place.   MainWindow (moved from root to solution directory of same name and then renamed)   Miner   Monitor   Setup Created MVVM View/ViewModels for Miner, Monitor, Setup. Will remove old Page versions in later commit Fixed project settings relating to manifest that were preventing successful builds

---
+++ MainWindow/MainWindowView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace Crossed_Miner
+{
+    /// <summary>
+    /// Interaction logic for MainWindowView.xaml
+    /// </summary>
+    public partial class MainWindowView : Window
+    {
+        public MainWindowView()
+        {
+            DataContext = new MainWindowViewModel();
+            InitializeComponent();
+
+            // Load Settings
+            Settings.LoadSettings();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void SetupButton_Click(object sender, RoutedEventArgs e)
+        {
+            Setup setupWindow = new Setup();
+            setupWindow.ShowDialog();
+        }
+    }
+}
